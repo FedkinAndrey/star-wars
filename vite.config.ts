fcb2db4de3ba90652fb2e5ccf0419beeb1f36d99
fedkin@bramblingtech.com <fedkin@bramblingtech.com>
2024-10-24 13:47:02 +0300

    fix app cors error

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,38 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-export default defineConfig({
-  plugins: [react()],
-  base: '/star-wars/',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  test: {
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
-    globals: true,
-  },
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), '');
+
+	const apiUrl = env.VITE_API_URL;
+
+	if (!apiUrl) {
+		console.error('VITE_API_URL is not defined in the .env file');
+		process.exit(1);
+	}
+
+	return {
+		plugins: [react()],
+		base: '/star-wars/',
+		resolve: {
+			alias: {
+				'@': path.resolve(__dirname, './src'),
+			},
+		},
+		server: {
+			proxy: {
+				'/api': {
+					target: apiUrl,
+					changeOrigin: true,
+					rewrite: (path) => path.replace(/^\/api/, ''),
+				},
+			},
+		},
+		test: {
+			environment: 'jsdom',
+			setupFiles: './src/setupTests.ts',
+			globals: true,
+		},
+	};
 });
